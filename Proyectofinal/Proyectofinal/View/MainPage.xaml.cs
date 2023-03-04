70fbfbcc08a2d9192ae8a7f39c864ca249f751ed
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-03-04 00:34:51 -0600

    LOGIN

--- Proyectofinal/Proyectofinal/View/MainPage.xaml.cs
+++ Proyectofinal/Proyectofinal/View/MainPage.xaml.cs
@@ -4,15 +4,25 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Proyectofinal
 {
     public partial class MainPage : ContentPage
     {
+
+        public ICommand TapCommand => new Command<string>(OpenBrowser);
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        [Obsolete]
+        public void OpenBrowser(string url)
+        {
+            Device.OpenUri(new Uri(url));
         }
     }
 }
