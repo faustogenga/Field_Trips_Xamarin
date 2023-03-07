8669f70d63352616e34490a562392f7d733007e8
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-03-07 13:16:49 -0600

    Update MainPage.xaml.cs

--- Proyectofinal/Proyectofinal/View/MainPage.xaml.cs
+++ Proyectofinal/Proyectofinal/View/MainPage.xaml.cs
@@ -12,17 +12,9 @@ namespace Proyectofinal
     public partial class MainPage : ContentPage
     {
 
-        public ICommand TapCommand => new Command<string>(OpenBrowser);
-
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        [Obsolete]
-        public void OpenBrowser(string url)
-        {
-            Device.OpenUri(new Uri(url));
         }
     }
 }
