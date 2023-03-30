9634e88649653160c4d582b85cfe842c541d9bc8
faustogengaalfaro@gmail.com <faustogengaalfaro@gmail.com>
2023-03-30 14:29:09 -0600

    update

--- Proyectofinal/Proyectofinal/ViewModel/CarreraViewModel.cs
+++ Proyectofinal/Proyectofinal/ViewModel/CarreraViewModel.cs
@@ -1,12 +1,7 @@
-﻿using Proyectofinal.DAL;
-using Proyectofinal.Model;
-using System;
+﻿using Proyectofinal.Model;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyectofinal.ViewModel
